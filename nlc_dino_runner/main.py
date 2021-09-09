219ad5c74d85fae60454a2f9ff0df40a81c16472
Miguel2774 <pre100503@gmail.com>
2021-09-08 20:22:40 -0500

    Clase 2: Ventana, background
    En la clase avanzamos nuestro código de Dino Runner:
    -Creación de la plantilla del código en pycharm.
    - Creación de clases: event, update y draw
    - Background (movimiento y posición)
    - Icono del juego
    - Título del juego
    -FPS clock
    -Subir png para usarlo como bg
    -Tamaño de ventana
    -Colores
    -Pequeños tips como: Control + Click izquierdo
    -Ejecución de la ventana y su correcto funcionamiento (ejemplo: que se cierre correctamente al terminar de jugar con la X)

--- nlc_dino_runner/main.py
+++ nlc_dino_runner/main.py
@@ -1,4 +1,9 @@
-import pygame
-print("testing commit")
+from nlc_dino_runner.Components.game import Game
+
+if __name__ == "__main__":
+    game = Game() #playing = False
+    game.run() #playing = True
+
+
 
 
