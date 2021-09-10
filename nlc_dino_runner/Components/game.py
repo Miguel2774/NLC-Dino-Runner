8e59ca11d613a915c870de75e51a795846f34133
Miguel2774 <pre100503@gmail.com>
2021-09-09 20:10:17 -0500

    Clase 3: Dino
    En esta clase vimos:
    - Personaje Dino (clase)
    - Posicionamiento de Dino
    - Animación de correr de Dino
    - Movimientos saltar y agacharse de Dino
    - Sprites y su concepto
    - Aplicación de programación orientada a objetos en el juego de Dino Runner.

--- nlc_dino_runner/Components/game.py
+++ nlc_dino_runner/Components/game.py
@@ -1,5 +1,6 @@
 import pygame
 
+from nlc_dino_runner.Components.dinosaur import Dinosaur
 from nlc_dino_runner.utils.constants import TITLE, ICON, SCREEN_WIDTH, SCREEN_HEIGHT, BG, FPS
 
 
@@ -14,6 +15,7 @@ class Game:
         self.x_pos_bg = 0
         self.y_pos_bg = 360
         self.game_speed = 20
+        self.player = Dinosaur()
 
     def run(self):
         self.playing = True
@@ -29,12 +31,14 @@ class Game:
                 self.playing = False
 
     def update(self):
-        pass
+        user_input = pygame.key.get_pressed()
+        self.player.update(user_input)
 
     def draw(self):
         self.clock.tick(FPS)
         self.screen.fill((255, 255, 255))
         self.draw_background()
+        self.player.draw(self.screen)
         pygame.display.update()
         pygame.display.flip()
 
