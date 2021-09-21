6001495d9f2e7aaf98d0538dc22fa4717dc5e87b
Miguel2774 <pre100503@gmail.com>
2021-09-21 18:41:36 -0500

    Clase 6..1 : Shield con tiempo de duraciónn

--- nlc_dino_runner/Components/dinosaur.py
+++ nlc_dino_runner/Components/dinosaur.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.sprite import Sprite
 from nlc_dino_runner.utils.constants import RUNNING, DUCKING, JUMPING, RUNNING_SHIELD, DUCKING_SHIELD, \
     JUMPING_SHIELD, DEFAULT_TYPE, SHIELD_TYPE
+from nlc_dino_runner.utils.text_utils import get_centered_message
 
 
 class Dinosaur(Sprite):
@@ -86,14 +87,23 @@ class Dinosaur(Sprite):
             self.dino_jump = False
             self.jump_vel = self.JUMP_VEL
 
-    # def check_invincibility(self, screen):
-    #     if self.shield:
-    #         time_to_show = round((self.shield_time_up - pygame.time.get_ticks())/1000, 3)
+    def check_invincibility(self, screen):
+        if self.shield:
+            time_to_show = round((self.shield_time_up - pygame.time.get_ticks())/1000, 3)
     #         # 0.24
-    #         if time_to_show >= 0:
-    #             self.show_text
-
-
+            if time_to_show < 0:
+                self.shield = False
+                if self.type == SHIELD_TYPE:
+                    self.type = DEFAULT_TYPE
+            else:
+                if self.show_text:
+                    text, text_rect = get_centered_message\
+                            (f"Shield enabled for {time_to_show}",
+                             width=500,
+                             height=40,
+                             size=20
+                        )
+                    screen.blit(text, text_rect)
 
     def draw(self, screen):
         screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
